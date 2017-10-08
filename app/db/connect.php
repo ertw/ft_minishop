7ade423a95c7e6ccd6c99e5b7ec58d824fcdc152
Erik Williamson <me@erik.tw>
2017-10-07 20:33:44 -0700

    start user verification

---
+++ app/db/connect.php
@@ -0,0 +1,3 @@
+<?php
+$db = pg_connect("host=db dbname=minishop_db password=my_password user=minishop_user options='--client_encoding=UTF8'");
+?>
--- app/front/login.php
+++ app/front/login.php
@@ -1,13 +1,21 @@
 <?php
 	require("../back/helper.php");
+	require_once("../db/connect.php");
 	if ($_SERVER["REQUEST_METHOD"] == "GET")
 		render("login_form.php", ["title"=>"Log In"]);
 	else if ($_SERVER["REQUEST_METHOD"] == "POST")
 	{
 		if (empty($_POST["user"]))
 			render("error.php", ["message"=>"Need a username."]);
-		elseif (empty($_POST["password"]))
+		if (empty($_POST["password"]))
 			render("error.php", ["message"=>"Need a password."]);
+		// let's query the db
+		$query = "SELECT password FROM minishop_db.users WHERE email = $1 LIMIT 1;";
+		$result = pg_query_params($db, $query, array($_POST["user"]));
+		if (!$result) {
+			render("error.php", ["message"=>"Critical DB error: unable to look up user."]);
+		}
+		render("error.php", ["message"=>"$result"]);
 		// query database for user information
 		// if user exists, check password
 		// should only have 1 row with said username, if more htan one then problem
