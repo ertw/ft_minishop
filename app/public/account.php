783fa051169a0dc4ad36820ef946f3ae864cd98e
terri <thuynh1989@gmail.com>
2017-10-08 15:34:56 -0700

    edits working on buy php and cart

--- app/public/account.php
+++ app/public/account.php
@@ -1,4 +1,5 @@
 <?php
 	require("../back/helper.php");
+	
 	render("settings.php", ["title"=>"Settings"]);
 ?>
--- app/views/shop_front.php
+++ app/views/shop_front.php
@@ -1,56 +1,74 @@
-  <div class="block">
-    <img src="../public/img/bread.png" style="top: 0; max-width: 70%" alt="Pusheen Bread"/>
-    <span>
-      <br />Pusheen Bread 
-      <br />$9000.00
-    </span>
-  </div>
-  <div class="block">
-    <img src="../public/img/pizza.png" style="top: 0; max-width: 90%" alt="Pusheen Cookie"/>
-    <span>
-      <br />Pusheen Pizza
-      <br />$9000.01
-    </span>
-  </div>
-  <div class="block">
-    <img src="../public/img/donut.png" style="max-width: 100%" alt="Pusheen Donut"/>
-    <span>
-      <br />Pusheen Donut
-      <br />$9000.02
-    </span>
-  </div>
-  <div class="block">
-    <img src="../public/img/ice.png" style="max-width: 100%" alt="Pusheen Ice Cream"/>
-    <span>
-      <br />Pusheen Ice Cream
-      <br />$9000.03
-    </span>
-  </div>
-  <div class="block">
-    <img src="../public/img/ramen.png" style="max-width: 80%" alt="Pusheen Ramen"/>
-    <span>
-      <br />Pusheen Ramen
-      <br />$9000.04
-    </span>
-  </div>
-  <div class="block">
-    <img src="../public/img/pancake.png" style="max-width: 70%" alt="Pusheen Pancake"/>
-    <span>
-      <br />Pusheen Pancake
-      <br />$9000.05
-    </span>
-  </div>
-  <div class="block">
-    <img src="../public/img/sushi.gif" style="top: 0; max-width: 85%" alt="Pusheen Sushi"/>
-    <span>
-      <br />Pusheen Sushi
-      <br />$9000.06
-    </span>
-  </div>
-  <div class="block">
-    <img src="../public/img/cupcake.png" style="max-width: 75%" alt="Pusheen Cupcake"/>
-    <span>
-      <br />Pusheen Cupcake
-      <br />$9000.07
-    </span>
-  </div>
+  <form action="buy.php" method="post">
+    <div class="block">
+      <img src="../public/img/bread.png" style="top: 0; max-width: 68%" alt="Pusheen Bread"/>
+      <span>
+        <br />Pusheen Bread 
+        <br />$9000.00
+        <br />
+        <input class="submit" type="submit" value="Buy Now"/>
+      </span>
+    </div>
+    <div class="block">
+      <img src="../public/img/pizza.png" style="top: 0; max-width: 88%" alt="Pusheen Cookie"/>
+      <span>
+        <br />Pusheen Pizza
+        <br />$9000.01
+        <br />
+        <input class="submit" type="submit" value="Buy Now"/>
+      </span>
+    </div>
+    <div class="block">
+      <img src="../public/img/donut.png" style="max-width: 97%" alt="Pusheen Donut"/>
+      <span>
+        <br />Pusheen Donut
+        <br />$9000.02
+        <br />
+        <input class="submit" type="submit" value="Buy Now"/>
+      </span>
+    </div>
+    <div class="block">
+      <img src="../public/img/ice.png" style="max-width: 97%" alt="Pusheen Ice Cream"/>
+      <span>
+        <br />Pusheen Ice Cream
+        <br />$9000.03
+        <br />
+        <input class="submit" type="submit" value="Buy Now"/>
+      </span>
+    </div>
+    <div class="block">
+      <img src="../public/img/ramen.png" style="top: 0; max-width: 78%" alt="Pusheen Ramen"/>
+      <span>
+        <br />Pusheen Ramen
+        <br />$9000.04      
+        <br />
+        <input class="submit" type="submit" value="Buy Now"/>
+      </span>
+    </div>
+    <div class="block">
+      <img src="../public/img/pancake.png" style="top: 0; max-width: 68%" alt="Pusheen Pancake"/>
+      <span>
+        <br />Pusheen Pancake
+        <br />$9000.05      
+        <br />
+        <input class="submit" type="submit" value="Buy Now"/>
+      </span>
+    </div>
+    <div class="block">
+      <img src="../public/img/sushi.gif" style="margin-top: -20px; max-width: 82%" alt="Pusheen Sushi"/>
+      <span>
+        <br />Pusheen Sushi
+        <br />$9000.06      
+        <br />
+        <input class="submit" type="submit" value="Buy Now"/>
+      </span>
+    </div>
+    <div class="block">
+      <img src="../public/img/cupcake.png" style="top: 0; max-width: 73%" alt="Pusheen Cupcake"/>
+      <span>
+        <br />Pusheen Cupcake
+        <br />$9000.07      
+        <br />
+        <input class="submit" type="submit" value="Buy Now"/>
+      </span>
+    </div>
+</form>
